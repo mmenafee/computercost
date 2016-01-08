9b1d53b37fb0821c66177a991fa075246341bf5e
mmenafee <mmenafee@deerfield.edu>
2016-01-08 01:06:04 -0500

    Added Cpu brands question
    Doesn't work, trying to integrate combobox and regular question, runs below and probably is incapable of doing combo box

--- Documents/workspace/compsci/ComputerCost/src/ComputerCost.java
+++ Documents/workspace/compsci/ComputerCost/src/ComputerCost.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JComboBox;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class ComputerCost
 {
@@ -5,7 +10,18 @@ public class ComputerCost
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
+		String cpu;
+		String [] cpubrands = {"Intel", "AMD"};
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println(Arrays.toString(cpubrands));
+		System.out.println("Type one of these brands:");
+		cpu =scan.nextLine();
+		
+		if(cpu == "Intel"){
+			 String[] models = { "Core i3", "Core i5","Core i7" };
+				JComboBox cpuModels = new JComboBox(models);
+		}
 	}
 
 }
